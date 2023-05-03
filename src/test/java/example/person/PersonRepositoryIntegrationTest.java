bf58e4c834d88d20177f8bb1ed5faa22555acf1b
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-05-02 23:49:29 -0300

    Changes to check H2 DB during test execution

--- src/test/java/example/person/PersonRepositoryIntegrationTest.java
+++ src/test/java/example/person/PersonRepositoryIntegrationTest.java
@@ -5,14 +5,32 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-@DataJpaTest
+/*
+In order to check the H2 database during the integration test execution (debug mode),
+you can access this URL:
+http://localhost:35707/h2-console
+Don't forget to change the port because it is random. Check the logs to get the correct port.
+You need also to change the JDBC URL to something like: jdbc:h2:mem:dfc57daf-4eb1-488a-a157-0f86cead685d
+Look at the logs to get the correct JDBC URL.
+
+You need also to add some properties to the application.properties file and
+verify that the breakpoints you set, suspend the Thread only and not all threads.
+For further information look here:
+https://hrrbrt.medium.com/using-h2-during-test-debugging-in-spring-f6a3db355e3a
+ */
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+//@DataJpaTest
 public class PersonRepositoryIntegrationTest {
 
     @Autowired
